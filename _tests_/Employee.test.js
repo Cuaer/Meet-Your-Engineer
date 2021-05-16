f288c16a9e1903ff7af30097dfb5c734dfed603c
Erick Cuapio <ecuapio92@gmail.com>
2021-05-15 23:10:09 -0700

    pushing all files

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,10 @@
+const employee = require ('../lib/employee.js');
+
+describe('employee', () => {
+    it ('should provide a string of information', () => {
+        const name = `${name}`;
+        const id = `${id}`;
+        const email = `${email}`;
+        const role = `${role}`;
+    });
+});
---
+++ index.js
@@ -0,0 +1,129 @@
+const fs = require('fs');
+const inquirer = require ('inquirer');
+
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+      },
+      {
+        type: 'input',
+        name: 'location',
+        message: 'Where are you from?',
+      },
+      {
+        type: 'input',
+        name: 'hobby',
+        message: 'What is your favorite hobby?',
+      },
+      {
+        type: 'input',
+        name: 'food',
+        message: 'What is your favorite food?',
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username',
+      },
+      {
+        type: 'input',
+        name: 'linkedin',
+        message: 'Enter your LinkedIn URL.',
+      },
+    ]);
+  };
+  
+  const generateHTML = (answers) =>
+    `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>Document</title>
+  </head>
+  <body>
+    <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Hi! My name is ${answers.name}</h1>
+      <p class="lead">I am from ${answers.location}.</p>
+      <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+      <ul class="list-group">
+        <li class="list-group-item">My GitHub username is ${answers.github}</li>
+        <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+      </ul>
+    </div>
+  </div>
+  </body>
+  </html>`;
+  
+  // Bonus using writeFileAsync as a promise
+const init = () => {
+    promptUser()
+      .then((answers) => writeFileAsync('index.html', generateHTML(answers)))
+      .then(() => console.log('Successfully wrote to index.html'))
+      .catch((err) => console.error(err));
+};
+  
+init();
+
+getName ();
+
+getId ();
+
+getEmail ();
+
+getRole(); //returns employee
+
+//Other three classes will **EXTEND** Employee
+
+//In addition to Employee's properties and methods, Manager will also have the following:
+
+// officeNumber
+
+// // getRole()—overridden to return 'Manager'
+// In addition to Employee's properties and methods, Engineer will also have the following:
+
+// github—GitHub username
+
+// getGithub()
+
+// getRole()—overridden to return 'Engineer'
+
+// In addition to Employee's properties and methods, Intern will also have the following:
+
+// school
+
+// getSchool()
+
+// getRole()—overridden to return 'Intern'
+
+// Finally, although it’s not a requirement, consider adding validation to ensure that user input is in the proper format.
+
+
+const generateHTML = (/*userinput*/) =>
+  `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Meet Your Engineer!</title>
+</head>
+<body>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Hi! M</h1>
+    <p class="lead">I am from ${answers.location}.</p>
+    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+    <ul class="list-group">
+      <li class="list-group-item">My GitHub username is ${answers.github}</li>
+      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>`;
